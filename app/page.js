654b081d3f3c6ce94fb9e5ac435418ef63d9bc21
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-14 22:32:31 +0100

    learning spacing

--- app/page.js
+++ app/page.js
@@ -4,27 +4,43 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">Hello, Tailwind!</h1>
-      <h2 className="text-2xl">Hello, Khaffy</h2>
-      <h3 className="text-xl">Welcome to your class</h3>
-      <p className="text-base">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
+      <div className="h-6 w-6 bg-purple-800 "></div>
+      <div className=" h-6 w-6 bg-pink-800 "></div>
+      <div className="h-6 w-36 bg-blue-800 opacity-40 "></div>
+      <div className=" bg-red-300 -mt-4 ">-mt-8</div>
+      <h1 className="text-3xl font-bold mb-2">This is a title</h1>
+      <p className="capitalize text-xl">Welcome to your class</p>
+      <div className="font-bold text-white m-4 bg-blue-500 py-1">
+        box with margin
+      </div>
+      <div className="text-3xl bg-slate-500 p-4">box with padding</div>
+      <h1 className="text-3xl mt-1">Hello, Tailwind!</h1>
+      <h2 className="text-2xl underline decoration-wavy decoration-2 decoration-red-600 underline-offset-4">
+        Hello, Khaffy
+      </h2>
+      <h3 className="capitalize text-xl">Welcome to your class</h3>
+      <p className="uppercase mb-4 text-base underline decoration-wavy underline-offset-4 leading-loose">
+        a regular paragraph
       </p>
-      <p className="text-sm">
+      <div className="break-all">
+        <p className="text-base mb-4  italic underline decoration-purple-500 decoration-1">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor pneumonoultramicroscopicsilicovolcanoconiosis
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </p>
+      </div>
+      <p className="text-sm mb-4">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt
         explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
         voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur,{' '}
+        quia dolor sit amet, consectetur
       </p>
+
       <p className="note text-xs">
         adipisci velit, sed quia non numquam eius modi tempora incidunt ut
         labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
