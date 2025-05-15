c4bd7fdcc73fc94db44b0341175078222685fccf
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-15 16:02:18 +0100

    learning flex

--- app/page.js
+++ app/page.js
@@ -4,7 +4,51 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div>
-      <div className="flex min-h-screen space-y-4">
+      <div className="parent">
+        <div className="bg-red-500 ">Header</div>
+        <div className="flex justify-center">
+          <div className=" bg-green-500 ">
+            <h1>Sidebar</h1>
+            <p>
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat
+            </p>
+          </div>
+          <div className=" bg-blue-500 ">
+            <h1>Main Content</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum."
+            </p>
+          </div>
+          <div className=" bg-blue-500 ">
+            <h1>Another Sidebar</h1>
+            <p>
+              {' '}
+              enim ad minim veniam, nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum."
+            </p>
+          </div>
+          {/* <div className="w-1/4 bg-green-500 ">Sidebar</div>
+          <div className="w-full bg-blue-500 ">Main Content</div>*/}
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex w-full flex-wrap">
+            <div className="w-full grow">01</div>
+            <div className="w-full grow ">02</div>
+            <div className="w-full grow">03</div>
+            <div className="w-full grow ">04</div>
+            <div className="w-full grow">05</div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="flex min-h-screen space-y-4">
         <div className="w-1/4   bg-purple-800 ">Sidebar</div>
         <div className=" w-4/5  bg-pink-800 ">Main-content</div>
       </div>
@@ -52,7 +96,7 @@ export default function Home() {
         reprehenderit qui in ea voluptate velit esse quam nihil molestiae
         consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
         pariatur?"
-      </p>
+      </p> */}
 
       {/* <h1 className="text-greeny mb-3 text-xs lg:text-3xl">Hello, Tailwind!</h1>
       <h1 className="bg-[#bada55]">Hello, Tailwind!</h1>
