7c906f1cfb0ccde9be2b3baf91a6bb79d260e8cb
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-17 18:54:03 +0100

    finished working on darkmode

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 // import Image from 'next/image';
 // import styles from './page.module.css';
@@ -6,15 +6,38 @@
 // import { useState } from 'react';
 // import { ArrowDown } from './arrow';
 
+//to test darkmode
 export default function Home() {
   return (
-    <html lang="en" className="dark">
-      <head />
-      <body className="dark:bg-slate-950 dark:text-white">Hello</body>
-    </html>
+    <div>
+      <h1 className="text-2xl font-bold">Welcome to my site!</h1>
+      <h2>What is Lorem Ipsum?</h2>{' '}
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.{' '}
+      </p>
+      <h2>Why do we use it?</h2>{' '}
+      <p>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).{' '}
+      </p>
+    </div>
   );
 }
-
 // export default function Home() {
 //   const [isOpen, setIsOpen] = useState(false);
 //   const [selectValue, setSelectValue] = useState('Select ');
--- components/icons/header.js
+++ components/icons/header.js
@@ -1,25 +1,33 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { Moon } from './icons/moon';
-import { Sun } from './icons/sun';
+import { useState, useEffect } from 'react';
+import { Sun } from './sun';
+import { Moon } from './moon';
 
-export default function header() {
+export default function Header() {
   const size = '24px';
-  const [darkMode, setDarkMode] =
-    (useState < boolean) | (undefined > undefined);
+  const [darkMode, setDarkMode] = useState(false);
 
   const switchMode = () => {
-    setDarkMode(!darkmode);
+    setDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode === null) {
+      setDarkMode(false); // default mode
+    } else {
+      setDarkMode(savedMode === 'true');
+    }
+  }, []);
 
   useEffect(() => {
     if (darkMode) {
       localStorage.setItem('darkMode', 'true');
-      window.document.documentElement.classList.add('dark');
-    } else if (darkMode === false) {
+      document.documentElement.classList.add('dark');
+    } else {
       localStorage.setItem('darkMode', 'false');
-      window.document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove('dark');
     }
   }, [darkMode]);
 
@@ -38,3 +46,42 @@ export default function header() {
     </header>
   );
 }
+
+// import { Moon } from './moon';
+// import { Sun } from './sun';
+
+// export default function Header() {
+//   const size = '24px';
+//   const [darkMode, setDarkMode] = useState(false);
+
+//   const switchMode = () => {
+//     setDarkMode(!darkmode);
+//   };
+
+//   useEffect(() => {
+//     if (darkMode) {
+//       localStorage.setItem('darkMode', 'true');
+//       window.document.documentElement.classList.add('dark');
+//     } else if (darkMode === false) {
+//       localStorage.setItem('darkMode', 'false');
+//       window.document.documentElement.classList.remove('dark');
+//     } else {
+//       setDarkMode(localStorage.getItem('darkMode') === 'true');
+//     }
+//   }, [darkMode]);
+
+//   return (
+//     <header className="flex items-center justify-end w-full p-4">
+//       <div
+//         className="transition cursor-pointer hover:text-blue-600"
+//         onClick={switchMode}
+//       >
+//         {!darkMode ? (
+//           <Moon width={size} height={size} />
+//         ) : (
+//           <Sun width={size} height={size} />
+//         )}
+//       </div>
+//     </header>
+//   );
+// }
