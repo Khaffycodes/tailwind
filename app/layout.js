175a492ccc390e24c0774d78a04583b9e90f3df8
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-17 18:56:02 +0100

    modified layout

--- app/layout.js
+++ app/layout.js
@@ -3,27 +3,6 @@ import './globals.css';
 
 import Header from '../components/icons/header';
 
-// import { useEffect, useState } from 'react';
-
-// export default function RootLayout({ children }) {
-//   const [darkMode, setDarkMode] = useState(false);
-
-//   useEffect(() => {
-//     const darkmode = localStorage.getItem('darkMode');
-//     setDarkMode(darkmode === 'true');
-//   }, []);
-
-//   return (
-//     <html lang="en">
-//       <head />
-//       <body className={darkMode ? 'dark bg-slate-950 text-white' : ''}>
-//         <Header />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
 export default function RootLayout({ children }) {
   return (
     <html
